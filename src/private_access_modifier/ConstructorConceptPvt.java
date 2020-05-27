f14818e203c1c55e6d27bad6d620d6047d741b01
Ayush Mathur <ayushmathur1994@gmail.com>
2020-05-28 00:45:34 +0530

    private constructor (Hide class)

---
+++ src/private_access_modifier/ConstructorConceptPvt.java
@@ -0,0 +1,9 @@
+package private_access_modifier;
+
+public class ConstructorConceptPvt {
+
+	private ConstructorConceptPvt() {
+		System.out.println("Hello");
+	}
+	
+}
---
+++ src/private_access_modifier/PrintConstructorConcPvt.java
@@ -0,0 +1,9 @@
+package private_access_modifier;
+
+public class PrintConstructorConcPvt {
+
+	public static void main(String[] args) {
+	//	ConstructorConceptPvt  constObj = new ConstructorConceptPvt();  //ConstructorConceptPvt() constructor is not visible i.e. class object / instance can not be make
+	}
+	
+}
