d7748e5a7d54b64551d77eda4ab22a468df469ea
Ayush Mathur <ayushmathur1994@gmail.com>
2020-05-27 21:09:42 +0530

    public

--- src/public_access_modifier/PrintPublicAccessDemo.java
+++ src/public_access_modifier/PrintPublicAccessDemo.java
@@ -5,7 +5,6 @@ public class PrintPublicAccessDemo {
 	public static void main(String args[]) {
 		PublicAccessModDemo publicDemoObj= new PublicAccessModDemo();
 		publicDemoObj.DisplayMyName();
-		
-	}
+		}
 	
 }
