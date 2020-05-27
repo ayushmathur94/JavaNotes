f7917934511a65475d971e08f3bb118d24ea4c5d
Ayush Mathur <ayushmathur1994@gmail.com>
2020-05-28 01:31:53 +0530

    default am commit

---
+++ src/default_access_modifier/onepackage/A.java
@@ -0,0 +1,9 @@
+package default_access_modifier.onepackage;
+
+class A {
+
+void displayme() {
+	System.out.println("default access modifier members are not accessible from other packages");;
+}
+	
+}
---
+++ src/default_access_modifier/otherpackage/PrintA.java
@@ -0,0 +1,10 @@
+package default_access_modifier.otherpackage;
+
+class PrintA {
+
+	public static void main(String args[]) {
+	//	A aObj = new A();    //error : A cannot be resolved to a type. 
+	//	aObj.displayme();
+	}
+	
+}
