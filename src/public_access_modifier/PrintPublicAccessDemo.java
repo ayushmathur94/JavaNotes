eb92ffc78f966b3d4e5d44e6784e4c21029b08d5
Ayush Mathur <ayushmathur1994@gmail.com>
2020-05-27 21:08:25 +0530

    Java Notes Commit V1.0
    Public Access Modifier

---
+++ src/public_access_modifier/PrintPublicAccessDemo.java
@@ -0,0 +1,10 @@
+package public_access_modifier;
+
+public class PrintPublicAccessDemo {
+
+	public static void main(String args[]) {
+		PublicAccessModDemo publicDemoObj= new PublicAccessModDemo();
+		publicDemoObj.DisplayMyName();
+	}
+	
+}
---
+++ src/public_access_modifier/PublicAccessModDemo.java
@@ -0,0 +1,10 @@
+package public_access_modifier;
+
+public class PublicAccessModDemo {
+
+	public void DisplayMyName() {
+		System.out.println("DBX");
+	
+	}
+	
+}
