7ba6d523f97b5a4066559dd6700dd759bb284232
Ayush Mathur <ayushmathur1994@gmail.com>
2020-06-19 20:00:56 +0530

    current class instance variables

---
+++ src/thiskeyword/pointone/ThiskeywordCurr.java
@@ -0,0 +1,28 @@
+package thiskeyword.pointone;
+
+
+public class ThiskeywordCurr {
+
+	int a;
+	int b;
+	
+	 // Parameterized constructor 
+	ThiskeywordCurr(int a, int b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	
+	void display() {
+		//Displaying value of variables a and b
+		System.out.println("a = " + a + " b = " + b);
+	}
+	
+	
+	public static void main(String[] args) {
+		ThiskeywordCurr object = new ThiskeywordCurr(10, 20);
+		
+		object.display();
+	}
+	
+}
